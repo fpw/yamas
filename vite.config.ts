7a61f1cce31723bcdfa26f3a243fd1df1bf1d612
Folke Will <folko@solhost.org>
2025-09-21 18:22:52 +0200

    Version 1.2.0 with reloc fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     },
     test: {
         globals: true,
+        exclude: [...configDefaults.exclude, "dist/"],
         coverage: {
             provider: "v8",
         },
